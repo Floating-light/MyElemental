8ea6a6292e8cc3dff10032e008ef80593df19d41
Floating-light <liuyuanyu12138@gmail.com>
2023-02-18 18:06:26 +0800

    init and niagra base concept

---
+++ Source/Elemental_III/Elemental_III.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Elemental_III.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Elemental_III, "Elemental_III" );
